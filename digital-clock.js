e464f313bf1f5451a3a53fd486942353fbfbffa9
Tracy <okorietracy8@gmail.com>
2023-01-27 02:30:28 +0100

    Digital clock code modified and working

--- digital-clock.js
+++ digital-clock.js
@@ -1,7 +1,7 @@
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minutes');
-const secondEl = document.querySelector('.seconds');
-const ampmEl = document.querySelector('.ampm');
+const hourEl = document.getElementById('hour');
+const minuteEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+const ampmEl = document.getElementById('ampm');
 
 function updateClock(){
     let h = new Date().getHours()
@@ -9,15 +9,22 @@ function updateClock(){
     let s = new Date().getSeconds()
     let ampm = 'AM'
 
-    if (h > 12){
+    if(h > 12){
         h = h - 12
         ampm = 'PM'
     }
 
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m: m;
+    s = s < 10 ? "0" + s : s;
+
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
-    ampm, (innerText = ampm);
+    ampmEl,(innerText = ampm);
+    setTimeout(()=>{
+        updateClock()
+    }, 1000)
 }
 
 updateClock();
